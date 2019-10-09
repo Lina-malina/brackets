555c79a12acecdb724c2bb40f637380f411a14dc
Angelina Pobyvanets <gela4w@gmail.com>
2019-10-09 20:56:07 +0300

    feat: implement brackets task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let sameBracketsStack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (bracketsConfig.some(bracket => bracket[0] == str[i] && bracket[1] == str[i])) {
+            if (sameBracketsStack.some(bracket => bracket == str[i])) {
+                let properBrackets = bracketsConfig.find(bracket => bracket[1] == str[i]);
+                let stackBracket = sameBracketsStack.pop();
+                if (properBrackets[0] != stackBracket) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(str[i]);
+                sameBracketsStack.push(str[i]);
+            }
+        } else if (bracketsConfig.some(bracket => bracket[0] == str[i])) {
+            stack.push(str[i]);
+        } else if (bracketsConfig.some(bracket => bracket[1] == str[i])) {
+            let properBrackets = bracketsConfig.find(bracket => bracket[1] == str[i]);
+            let stackBracket = stack.pop();
+            if (properBrackets[0] != stackBracket) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length == 0 && sameBracketsStack.length == 0;
 }
+
